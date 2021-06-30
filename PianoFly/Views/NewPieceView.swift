27afbf5f48256a60b34b21b96f59eba785a02efc
Derek Hsieh <devderekhsieh@gmail.com>
2021-06-29 22:21:16 -0700

    update gitignore file with sketchc file

---
+++ PianoFly/Views/NewPieceView.swift
@@ -0,0 +1,20 @@
+//
+//  NewPieceView.swift
+//  PianoFly
+//
+//  Created by Derek Hsieh on 6/29/21.
+//
+
+import SwiftUI
+
+struct NewPieceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewPieceView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPieceView()
+    }
+}
